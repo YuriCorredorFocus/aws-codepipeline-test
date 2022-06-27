d3804330089658bca1594421033d76284f9fd2b8
YuricorredorFocus <Yuri.corredor@focusnfe.com.br>
2022-06-27 11:17:16 -0300

    counter app with tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render } from '@testing-library/react'
+import App from './App'
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('Counter works properly', () => {
+  const { getByTestId, getByText } = render(<App />)
+  const counterValue = getByTestId("counter-value")
+  expect(counterValue).toHaveTextContent("0")
+
+  const plusButton = getByText("+1", { selector: "button" })
+  const minusButton = getByText("-1", { selector: "button" })
+
+  fireEvent.click(plusButton)
+  expect(counterValue).toHaveTextContent("1")
+
+  fireEvent.click(minusButton)
+  fireEvent.click(minusButton)
+  expect(counterValue).toHaveTextContent("-1")
+})
